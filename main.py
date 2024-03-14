d0b9b00e9e3bcdcd8015f82cf655b517380d58f8
Its.alexito__dev <110727609+itsalexitodev@users.noreply.github.com>
2024-03-14 18:54:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from Bingo import *
 
 def main():
     while True:
-        print("\n -Menu- \n")
-        print("1. Comprar caron")
-        print("2. Ver Carton")
-        print("3. Bombo")
-        print("4. Numeros sorteados")
-        print("5. Creditos")
-        print("")
+        print("#" *25)
+        print("#        Bingo          #")
+        print("#         by            #")
+        print("# Alex Garcia Rodriguez #")
+        print("#" *25)
+
+        print("\n-Menu-\n")
+        print("- 1. Comprar cartón -")
+        print("- 2. Ver Cartón -")
+        print("- 3. Bombo -")
+        print("- 4. Números sorteados -")
+        print("- 5. Salir -")
+
+
 
 if __name__ == "__main__":
     main()
