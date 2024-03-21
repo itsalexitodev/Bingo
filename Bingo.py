d17d4adaca10050ac858ecfad96f5d31a61ee075
Its.alexito__dev <110727609+itsalexitodev@users.noreply.github.com>
2024-03-21 15:01:29 +0100

    def bingo en construcion

--- Bingo.py
+++ Bingo.py
@@ -33,32 +33,4 @@ def ver_cartones(cartones):
         print()  # Imprimir una línea en blanco entre cartones
 
 def bombo(cartones, bola, numeros):
-    numeros_ganadores = set()
-    lineas_premiadas = set()
-    ganadores_bingo = []
-
-    numeros.append(bola)
-
-    for id_carton, carton in cartones:
-        for fila_idx, fila in enumerate(carton):
-            if all(num == 'X' for num in fila):
-                continue  # Si la fila ya está completa, pasamos a la siguiente
-            if bola in fila:
-                columna_idx = fila.index(bola)
-                carton[fila_idx][columna_idx] = 'X'
-
-                # Verificar si la fila ahora está completa
-                if all(num == 'X' for num in fila):
-                    lineas_premiadas.add((id_carton, fila_idx + 1))
-                    print(f"Línea completa! La línea {fila_idx + 1} del cartón {id_carton} ha sido premiada con 20€.")
-
-                # Verificar si el cartón ahora está completo
-                if all(all(num == 'X' for num in fila) for fila in carton):
-                    ganadores_bingo.append(id_carton)
-                    carton_premiado = '\n'.join([' '.join(map(str, fila)) for fila in carton])
-                    print(f"¡Bingo! El cartón {id_carton} ha cantado bingo.\nCartón ganador:\n{carton_premiado}")
-                    premio_total = (len(lineas_premiadas) + 1) * 20 + len(ganadores_bingo) * 100
-                    print(f"Cantidad ganada: {premio_total}€")
-                    return cartones
-
-    return cartones
+   pass
